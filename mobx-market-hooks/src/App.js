92a04a9498e75f1502d19262f68589924fe7f8bc
zwonH <a.circle.one@gmail.com>
2020-10-22 15:00:45 +0900

    feat: counter 스토어 생성

--- mobx-market-hooks/src/App.js
+++ mobx-market-hooks/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import Counter from './components/Counter';
+
 // 카운터 뷰 컴포넌트 불러와야지
 
 function App() {
   return (
     <div className="App">
-      <Counter />
+      <text>mobX with hooks</text>
     </div>
   );
 }
--- mobx-market-hooks/src/stores/counter.js
+++ mobx-market-hooks/src/stores/counter.js
@@ -1,6 +1,5 @@
-// market 데이터 스토어도 만들 것이기 때문에 stores라는 폴더 생성.
-
 import { observable } from 'mobx';
+// observable: 관찰 받고 있는 state
 
 const counter = observable({
   number: 1,
